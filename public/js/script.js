358ade71720cbb49b5531aef713d9661ba9d422d
Jesus Ruiz Gutierrez <jesse432007@gmail.com>
2024-07-19 14:57:08 -0700

    Resolved merge conflict in public/html/index.html

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,27 @@
-const loginPopUp = document.querySelector('.pop-up-login')
-const createAccountLink = document.querySelector('.create-account-link')
-const loginBtn = document.querySelector('.login-btn');
-const closeBtn = document.querySelector('.close-btn');
+const loginPopUp = document.querySelector(".pop-up-login");
+const createAccountLink = document.querySelector(".create-account-link");
+const loginBtn = document.querySelector(".login-btn");
+const closeBtn = document.querySelector(".close-btn");
+const showCreateAccount = document.querySelector('#showCreateAccount');
+const createAccount = document.querySelector('.create-account')
 
-loginBtn.addEventListener('click', function(){
-    console.log("Login btn clicked");
-    loginPopUp.style.display = "block";
-})
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
 
-closeBtn.addEventListener('click', function(){
-    loginPopUp.style.display = "none";
-})
+loginBtn.addEventListener("click", function () {
+  console.log("Login btn clicked");
+  loginPopUp.style.display = "block";
+});
 
+closeBtn.addEventListener("click", function () {
+  loginPopUp.style.display = "none";
+});
 
-createAccountLink.addEventListener('click', () => {
-    loginPopUp.classList.add('active')
+createAccountLink.addEventListener("click", () => {
+  loginPopUp.classList.add("active");
+});
+
+showCreateAccount.addEventListener('click', function(){
+  createAccount.style.display = "block";
 })
