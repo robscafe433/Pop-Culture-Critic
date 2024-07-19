180b93bbb161d66c1b17851ead0b6aa0a7d89aaa
geovko <octobermes@gmail.com>
2024-07-18 21:30:08 -0700

    edited user model

--- models/User.js
+++ models/User.js
@@ -20,9 +20,13 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    role: {
+    email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
     password: {
       type: DataTypes.STRING,
