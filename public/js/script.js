df7b548c82b069987559bb723f4b7046045ac9d0
Jesus Ruiz Gutierrez <jesse432007@gmail.com>
2024-07-21 16:26:36 -0700

    Login and create account popup

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,40 @@
 const loginPopUp = document.querySelector(".pop-up-login");
-const createAccountLink = document.querySelector(".create-account-link");
 const loginBtn = document.querySelector(".login-btn");
 const closeBtn = document.querySelector(".close-btn");
-const showCreateAccount = document.querySelector('#showCreateAccount');
-const createAccount = document.querySelector('.create-account')
-
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
+const showCreateAccountFormLink = document.getElementById(
+  "showCreateAccountForm"
+);
+const showLoginFormLink = document.getElementById("showLoginForm");
+const loginFormContainer = document.getElementById("loginFormContainer");
+const createAccountFormContainer = document.getElementById(
+  "createAccountFormContainer"
+);
 
 loginBtn.addEventListener("click", function () {
-  console.log("Login btn clicked");
-  loginPopUp.style.display = "block";
+  loginPopUp.style.display = "flex";
+  loginFormContainer.classList.add("active");
+  createAccountFormContainer.classList.remove("active");
 });
 
 closeBtn.addEventListener("click", function () {
   loginPopUp.style.display = "none";
 });
 
-createAccountLink.addEventListener("click", () => {
-  loginPopUp.classList.add("active");
+window.addEventListener("click", function (event) {
+  if (event.target == loginPopUp) {
+    loginPopUp.style.display = "none";
+  }
 });
 
-showCreateAccount.addEventListener('click', function(){
-  createAccount.style.display = "block";
-})
+showCreateAccountFormLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  loginFormContainer.classList.remove("active");
+  createAccountFormContainer.classList.add("active");
+  createAccountFormContainer.style.display = "block";
+});
+
+showLoginFormLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  createAccountFormContainer.classList.remove("active");
+  loginFormContainer.classList.add("active");
+});
