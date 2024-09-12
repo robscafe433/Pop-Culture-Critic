f9acb35bddc06bf6628dc61aafdf6fb4a91a6507
geovko <octobermes@gmail.com>
2024-09-12 00:20:38 -0700

    port change to 5432

--- config/connection.js
+++ config/connection.js
@@ -10,8 +10,8 @@ const sequelize = process.env.DB_URL
       process.env.DB_PASSWORD,
       {
         host: "localhost",
-        port: 3306,
-        //port: 5432,
+        // port: 3306,
+        port: 5432,
         dialect: "postgres",
       }
     );
